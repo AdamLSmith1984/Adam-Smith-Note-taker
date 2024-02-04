012880099f3d3e7adef608f65f199825d97699c4
Adam Smith <asmith5144@hotmail.com>
2024-02-04 11:34:53 -0500

    add delete function to api.js

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
+const path = require("path");
 const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
+const notesDBPath = path.join(__dirname, "../db/db.json");
 
 router.get('/api/notes', async (req, res) => {
     const dbJson = await JSON.parse(fs.readFileSync("db/db.json", "utf8"));
@@ -18,5 +20,12 @@ router.post('/api/notes', (req, res) => {
     fs.writeFileSync("db/db.json", JSON.stringify(dbJson));
     res.json(dbJson);
 });
+router.delete("/api/notes/:id", (req, res) => {
+    const noteId = req.params.id;
+    const json = JSON.parse(fs.readFileSync(notesDBPath));
+    const result = json.filter((note) => note.id !== noteId);
+    fs.writeFileSync(notesDBPath, JSON.stringify(result));
+    res.json(`Item ${noteId} has been deleted.`);
+  });
 
 module.exports = router;
